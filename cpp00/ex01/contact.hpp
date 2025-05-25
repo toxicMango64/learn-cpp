4c511a051122d51a438ba3800eb8cb9c61bb9c2e
toxicMango64 <danielfortesque666+github@gmail.com>
2025-05-25 13:54:45 +0400

    minor changes for readability and removed whitespaces

--- cpp00/ex01/contact.hpp
+++ cpp00/ex01/contact.hpp
@@ -1,5 +1,4 @@
-#ifndef CONTACT_HPP
-# define CONTACT_HPP
+#pragma once
 
 # include <string>
 # include <iostream>
@@ -14,29 +13,28 @@ const string GREEN = "\033[32m";
 const string YELLOW = "\033[33m";
 
 class Contact {
-	private:
-		string	_firstName;
-		string	_lastName;
-		string	_nickname;
-		string	_phoneNumber;
-		string	_darkestSecret;
-	public:
-		Contact();
-		~Contact();
-		void		setFirstName(string firstName);
-		void		setLastName(string lastName);
-		void		setNickname(string nickname);
-		void		setPhoneNumber(string phoneNumber);
-		void		setDarkestSecret(string darkestSecret);
-		string		getFirstName();
-		string		getLastName();
-		string		getNickname();
-		string		getPhoneNumber();
-		string		getDarkestSecret();
-		void		setContact();
-		void		setInput(const string& prompt, void (Contact::*setter)(const string));
-		void		clearInput(string& input);
-		void		printContact();
-};
+private:
+	string	_firstName;
+	string	_lastName;
+	string	_nickname;
+	string	_phoneNumber;
+	string	_darkestSecret;
 
-#endif
+public:
+	Contact();
+	~Contact();
+	void		setFirstName(string firstName);
+	void		setLastName(string lastName);
+	void		setNickname(string nickname);
+	void		setPhoneNumber(string phoneNumber);
+	void		setDarkestSecret(string darkestSecret);
+	string		getFirstName();
+	string		getLastName();
+	string		getNickname();
+	string		getPhoneNumber();
+	string		getDarkestSecret();
+	void		setContact();
+	void		setInput(const string& prompt, void (Contact::*setter)(const string));
+	void		clearInput(void);
+	void		printContact();
+};
