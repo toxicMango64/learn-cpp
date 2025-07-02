a1d754b41062829466589c95306133eae97fa5bb
toxicmango64 <danielfortesque666+github@protonmail.com>
2025-07-02 19:19:38 +0400

    fix: code is more readable and less bug prone

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -17,8 +17,9 @@ BitcoinExchange::~BitcoinExchange() {}
 
 BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &other)
 {
-	if (this != &other)
+	if (this != &other) {
 		_price_history = other._price_history;
+	}
 	return *this;
 }
 
@@ -29,18 +30,20 @@ void BitcoinExchange::feed(const std::string &filename)
 	std::istringstream	iss;
 	std::string			date;
 	std::string			price;
-	double				price_value;
+	double				price_value = 0;
 
-	if (file.is_open() == false)
+	if (file.is_open() == false) {
 		throw CantOpenFileException();
+	}
 	while (std::getline(file, line))
 	{
 		date = line.substr(0, line.find(','));
 		price = line.substr(line.find(',') + 1);
 		iss.str(price);
 		iss >> price_value;
-		if (iss.fail() == true)
+		if (iss.fail() == true) {
 			price_value = 0;
+		}
 		iss.clear();
 		_price_history[date] = price_value;
 	}
@@ -54,27 +57,31 @@ void BitcoinExchange::convertToValues(const std::string &input_file) const
 	std::string			line;
 	std::string			date;
 	std::string			amount_str;
-	double				amount;
-	double				transaction_value;
+	double				amount = 0;
+	double				transaction_value = 0;
 
-	if (file.is_open() == false)
+	if (file.is_open() == false) {
 		throw CantOpenFileException();
-	while (std::getline(file, line))
+	}
+	while (std::getline(file, line) && !line.empty())
 	{
-		if (check_line_validity(line) == false)
-			continue;
+		if (check_line_validity(line) == false) {
+			continue ;
+		}
 		date = line.substr(0, line.find(' '));
-		if (check_date_validity(date) == false)
-			continue;
+		if (check_date_validity(date) == false) {
+			continue ;
+		}
 		amount_str = line.substr(line.find('|') + 1);
 		iss.clear();
 		iss.str(amount_str);
 		iss >> amount;
-		if (check_amount_validity(amount, amount_str, iss) == false)
-			continue;
+		if (check_amount_validity(amount, amount_str, iss) == false) {
+			continue ;
+		}
 		std::cout << std::setprecision(2) << std::fixed;
 		transaction_value = amount * getClosestPriceAtDate(date);
-		std::cout << date << " => " << transaction_value << std::endl;
+		std::cout << date << " => " << transaction_value << "\n";
 	}
 	file.close();
 }
@@ -83,7 +90,7 @@ bool	check_line_validity(const std::string &line)
 {
 	if (line.empty() || line.find('|') == std::string::npos || line.find(' ') == std::string::npos)
 	{
-		std::cerr << "Error: bad input => '" << line << "'" << std::endl;
+		std::cerr << "Error: bad input => '" << line << "'\n";
 		return false;
 	}
 	return true;
@@ -93,12 +100,12 @@ bool	check_date_validity(const std::string &date)
 {
 	if (date.size() != 10 || date[4] != '-' || date[7] != '-')
 	{
-		std::cerr << "Error: bad date format => '" << date << "'" << std::endl;
+		std::cerr << "Error: bad date format => '" << date << "'\n";
 		return false;
 	}
 	if (date.substr(5, 2) > "12" || date.substr(8, 2) > "31")
 	{
-		std::cerr << "Error: bad date format => '" << date << "'" << std::endl;
+		std::cerr << "Error: bad date format => '" << date << "'\n";
 		return false;
 	}
 	return true;
@@ -108,17 +115,17 @@ bool	check_amount_validity(double amount, std::string &amount_str, std::istrings
 {
 	if (iss.fail() == true)
 	{
-		std::cout << "Error: not a number => '" << amount_str << "'" << std::endl;
+		std::cout << "Error: not a number => '" << amount_str << "'\n";
 		return false;
 	}
 	if (amount < 0)
 	{
-		std::cout << "Error: not a positive number => '" << amount_str << "'" << std::endl;
+		std::cout << "Error: not a positive number => '" << amount_str << "'\n";
 		return false;
 	}
 	if (amount >= std::numeric_limits<int>::max())
 	{
-		std::cout << "Error: number too large => '" << amount_str << "'" << std::endl;
+		std::cout << "Error: number too large => '" << amount_str << "'\n";
 		return false;
 	}
 	return true;
@@ -129,11 +136,13 @@ double BitcoinExchange::getClosestPriceAtDate(const std::string &date) const
 	std::map<std::string, double>::const_iterator it;
 
 	it = _price_history.find(date);
-	if (it != _price_history.end()) //found the exact date
+	if (it != _price_history.end()) {/** found the exact date */
 		return it->second;
-	it = _price_history.lower_bound(date); //take the first element that's not less than date
-	if (it == _price_history.begin()) //date is before the first date in the csv
+	}
+	it = _price_history.lower_bound(date); /** take the first element that's not less than date */
+	if (it == _price_history.begin()) { /* date is before the first date in the csv */
 		return 0;
+	}
 	return (--it)->second; //returns the price just before the date found
 }
 
--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,9 +6,10 @@ int main(int argc, char **argv)
 {
 	try
 	{
-		if (check_args(argc, argv) == 1)
+		if (check_args(argc, argv) == 1) {
 			return 1;
-		BitcoinExchange exchange(DATA_FILE);
+		}
+		const BitcoinExchange exchange(DATA_FILE);
 		exchange.convertToValues(argv[1]);
 	}
 	catch(const std::exception& e)
@@ -21,12 +22,12 @@ static int8_t	check_args(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
+		std::cerr << "Usage: " << argv[0] << " <input_file>\n";
 		return 1;
 	}
 	if (std::string(argv[1]).find(".csv") == std::string::npos)
 	{
-		std::cerr << "Error: input file must be a .csv file" << std::endl;
+		std::cerr << "Error: input file must be a .csv file\n";
 		return 1;
 	}
 	return 0;
